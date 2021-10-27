a1e725ce234838af55b55a713cb72212a0ab5d12
Keiichi Enomoto <enm10k@gmail.com>
2021-10-27 19:51:03 +0900

    build.ios.sh のターゲットに simulator:arm64 を追加する

--- build.ios.sh
+++ build.ios.sh
@@ -14,6 +14,7 @@ set -ex
 # ======= ここまでは全ての build.*.sh で共通（PACKAGE_NAME だけ変える）
 
 TARGET_ARCHS="arm64 x64"
+XCFRAMEWORK_TARGET_ARCHS="arm64 x64 simulator:arm64"
 TARGET_BUILD_CONFIGS="debug release"
 
 ./scripts/get_depot_tools.sh $SOURCE_DIR
@@ -56,7 +57,7 @@ pushd $SOURCE_DIR/webrtc/src
     # M94 で use_xcode_clang=true かつ --bitcode を有効にしてビルドが通り bitcode が有効になってることを確認
     # M95 で再度 clang++: error: -gdwarf-aranges is not supported with -fembed-bitcode エラーがでるようになった
     # https://webrtc-review.googlesource.com/c/src/+/232600 が影響している可能性があるため use_lld=false を追加
-    ./tools_webrtc/ios/build_ios_libs.sh -o $BUILD_DIR/webrtc/$build_config --build_config $build_config --arch $TARGET_ARCHS --bitcode --extra-gn-args " \
+    ./tools_webrtc/ios/build_ios_libs.sh -o $BUILD_DIR/webrtc/$build_config --build_config $build_config --arch $XCFRAMEWORK_TARGET_ARCHS --bitcode --extra-gn-args " \
       use_xcode_clang=true \
       use_lld=false \
       rtc_libvpx_build_vp9=true \
